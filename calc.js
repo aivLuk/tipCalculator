cca3c034407d47ac2e8c09b120571f168fa3ee85
Aivaras <lukauskasaivaras@gmail.com>
2019-11-27 16:28:59 +0100

    tipCalculator 1st  draft

---
+++ calc.js
@@ -0,0 +1,7 @@
+// paimti bill size i var
+// paimti zmoniu kieki i var
+// paimti service i var
+// paimti i var calculated value
+// sugalvoti 3 funkcijas su skirtingais service suskaiciuoti tipsus
+// setupint eventclickeri buttonui
+// display new calculated textContent
