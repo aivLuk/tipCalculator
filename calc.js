317d3fbadd71ed899ea7f465d5b39b73fc32cbd5
Aivaras <lukauskasaivaras@gmail.com>
2019-11-29 07:04:42 +0100

    select value left to fix

--- calc.js
+++ calc.js
@@ -1,6 +1,19 @@
+var service = document.querySelector("select").value;
 var calculate = document.querySelector("button");
 var h3 = document.querySelector("h3");
 
 calculate.addEventListener("click", function() {
-  h3.textContent = "So and so tips";
+  var billSize = parseInt(document.querySelector(".bill").value);
+  var people = parseInt(document.querySelector(".people").value);
+  var myBill = billSize / people;
+  if (service === "bad") {
+    tips = myBill * 0.05;
+    h3.textContent = "You should leave " + tips + " $ for a tip.";
+  } else if (service === "good") {
+    tips = myBill * 0.1;
+    h3.textContent = "You should leave " + tips + " $ for a tip.";
+  } else if (service === "great") {
+    tips = myBill * 0.15;
+    h3.textContent = "You should leave " + tips + " $ for a tip.";
+  }
 });
